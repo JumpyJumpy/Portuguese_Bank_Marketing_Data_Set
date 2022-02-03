58c130d32668457d8be9638f1b00fbc2b6c9cdb6
Shubo Lin <slin268@wisc.edu>
2022-02-03 16:26:33 -0600

    update

--- code/preprocess.py
+++ code/preprocess.py
@@ -8,14 +8,16 @@ bank = pd.read_csv("./data/Casestudy Data.csv")
 
 bank.describe()
 bank.info()
+
+bank = bank.replace("unknown", value = np.nan)
+
 bank_X = bank.drop(columns = ["y"])
 bank_y = bank["y"]
-
 cate_features = []
 
 for col in bank_X.columns:
     if bank_X[col].dtype == "object":
-        bank_X[col] = bank_X[col].astype("category")
+        # bank_X[col] = bank_X[col].astype("category")
         cate_features.append(col)
 
 bank_X.info()
@@ -24,13 +26,12 @@ np.sum(bank_X.isna())
 le = LabelEncoder()
 mapping = {}
 for col in cate_features:
-    le.fit(bank_X[col])
+    le.fit(bank_X[col][bank_X[col].notna()])
     levels = list(le.classes_)
     mapping[col] = {i: le.transform([i]).tolist()[0] for i in levels}
 
-    bank_X.loc[bank_X[col][bank_X[col].notnull()].index, col] = pd.Series(le.transform(bank_X[col][bank_X[col].notnull()]),
-    index = bank_X[col][bank_X[col].notnull()].index)
-
+    bank_X.loc[bank_X[col][bank_X[col].notna()].index, col] = \
+        pd.Series(le.transform(bank_X[col][bank_X[col].notna()]), index = bank_X[col][bank_X[col].notna()].index)
 
 np.sum(bank_X.isna())
 bank_X.describe()
@@ -42,5 +43,6 @@ bank_X_imputed_knn = pd.DataFrame(imputer.fit_transform(bank_X), columns = bank_
 bank_X_imputed_knn.to_csv("bank_X_imputed_knn.csv")
 
 import json
+
 with open("mapping.json", "w") as file:
     file.write(json.dumps(mapping))
