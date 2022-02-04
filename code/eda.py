8279edf9f2cc59fb7b92e95041965b4a06bc70f3
Shubo Lin <slin268@wisc.edu>
2022-02-03 22:56:59 -0600

    update

--- code/eda.py
+++ code/eda.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 bank_X_flattened = pd.read_csv("./data/bank_X_flattened.csv")
 
 
-plt.figure(figsize = (16, 16))
+plt.figure(figsize = (20, 16))
+sn.set(font_scale = 2)
 sn.heatmap(bank_X_flattened.corr(), cmap = "YlGnBu", linewidth = 1)
 plt.show()
